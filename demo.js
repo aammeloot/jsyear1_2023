d493d7d3cd823496b244d7b8008ee73da4dcc484
Aurélien Ammeloot <559069+aammeloot@users.noreply.github.com>
2023-09-25 16:03:30 +0100

    All files covered in class weeks 1 - 5

---
+++ demo.js
@@ -0,0 +1,4 @@
+alert("Hello World");
+let age = prompt("How old are you?");
+console.log("You are " + age + " years old.");
+
---
+++ lab1.js
@@ -0,0 +1,22 @@
+// Declare the variables
+
+// the names
+var name1; // Declaring variable
+name1 = "Maxwell Wright"; // Initialising variable
+var name2 = "Raja Villarreal"; // Both steps on a single line
+var name3 = "Helen Richards";
+
+// the phone numbers
+var phone1 = "(0191) 719 6495";
+var phone2 = "0866 398 2895";
+var phone3 = "0800 1111";
+
+// emails
+
+var email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+var email2 = "posuere.vulputate@sed.com";
+var email3 = "libero@convallis.edu";
+
+alert(name1 + "/" + phone1 + "/" + email1);
+alert(name2 + "/" + phone2 + "/" + email2);
+alert(name3 + "/" + phone3 + "/" + email3);
